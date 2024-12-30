f246fb1d2866393c901089fd9e0f09b91d7bfcbe
Mplus <mydiv.1982@gmail.com>
2024-12-30 20:25:39 +0100

    edit script.js

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ document.querySelector('.score').textContent = score;
 
 let highscoreEl = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!';
+    displayMessage('No Number!');
   } else if (guess === secretNumber) {
     document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number!');
     document.querySelector('body').style.background = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     if (score > highscoreEl) {
@@ -23,65 +27,28 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   } else if (guess !== secretNumber) {
     if (score > 0) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? 'Too High!' : 'Too Low!';
+      displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
       document.querySelector('.guess').value = '';
       document.querySelector('.guess').focus();
       score--;
       if (score === 0) {
         document.querySelector('.number').textContent = secretNumber;
-        document.querySelector('.message').textContent = 'You lost the Game!';
+        displayMessage('You lost the Game!');
         document.querySelector('body').style.background = 'red';
       }
     } else {
       document.querySelector('.number').textContent = secretNumber;
-      document.querySelector('.message').textContent = 'You lost the Game!';
+      displayMessage('You lost the Game!');
     }
   }
   document.querySelector('.score').textContent = score;
 });
-//   else if (guess > secretNumber) {
-//     if (score > 0) {
-//       document.querySelector('.message').textContent = 'Too High!';
-//       document.querySelector('.guess').value = '';
-//       document.querySelector('.guess').focus();
-//       // score = score - 1;
-//       score--;
-//       if (score === 0) {
-//         document.querySelector('.number').textContent = secretNumber;
-//         document.querySelector('.message').textContent = 'You lost the Game!';
-//         document.querySelector('body').style.background = 'red';
-//       }
-//     } else {
-//       document.querySelector('.number').textContent = secretNumber;
-//       document.querySelector('.message').textContent = 'You lost the Game!';
-//     }
-//   } else if (guess < secretNumber) {
-//     if (score > 0) {
-//       document.querySelector('.message').textContent = 'Too Low!';
-//       document.querySelector('.guess').value = '';
-//       document.querySelector('.guess').focus();
-//       // score = score - 1;
-//       score--;
-//       if (score === 0) {
-//         document.querySelector('.number').textContent = secretNumber;
-//         document.querySelector('.message').textContent = 'You lost the Game!';
-//         document.querySelector('body').style.background = 'red';
-//       }
-//     } else {
-//       document.querySelector('.number').textContent = secretNumber;
-//       document.querySelector('.message').textContent = 'You lost the Game!';
-//     }
-//   }
-//   document.querySelector('.score').textContent = score;
-// });
-
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.background = 'black';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '?';
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   score = 20;
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
